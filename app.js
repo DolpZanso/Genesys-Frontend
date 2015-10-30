dec3afab54ab2b767b99723f8e21d95116e44774
Romelio Tavas <romeliotavas@gmail.com>
2015-10-30 15:11:08 +0800

    Added multer to middlewares

--- app.js
+++ app.js
@@ -1,33 +1,37 @@
 var express = require('express');
 var path = require('path');
-//var favicon = require('serve-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var multer  = require('multer');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var sceneObjects = require('./routes/sceneObjects');
 
 
-
 var app = express();
+
+var upload = multer({ dest: 'uploads/' });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', routes);
 app.use('/users', users);
-app.use('/sceneObjects',sceneObjects);
+
+var cpUpload = upload.fields([{ name: 'objThumbnail', maxCount: 1 }, { name: 'objFile', maxCount: 1 }]);
+app.use('/sceneObjects',cpUpload,sceneObjects);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/sceneObjects.js
+++ routes/sceneObjects.js
@@ -4,13 +4,10 @@ var multer  = require('multer');
 var router = express.Router();
 var SceneObject = require('../models/SceneObject');
 
-var upload = multer({ dest: 'uploads/' });
 
-var cpUpload = upload.fields([{ name: 'objThumbnail', maxCount: 1 }, { name: 'objFile', maxCount: 1 }]);
 
-router.post('/upload',cpUpload, function(req, res, next) {
+router.post('/upload', function(req, res, next) {
 
-  //TODO: GET request objects from req
   console.log("Printing files");
   console.log(req.files);
 
